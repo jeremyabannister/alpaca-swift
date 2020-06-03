1943af1960c1f0d086ecabc3f1de8834ddc8926f
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2020-06-03 01:08:06 -0400

    Added a product to `Package.swift`

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
 	name: "Alpaca",
+    products: [
+        .library(name: "Alpaca", targets: ["Alpaca"])
+    ],
 	targets: [
 		.target(name: "Alpaca"),
 		.testTarget(name: "Tests", dependencies: ["Alpaca"])
